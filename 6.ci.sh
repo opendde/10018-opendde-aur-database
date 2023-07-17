4c2dd414c9ab99528fa02c16a6a75a68c41e580f
8Mi_Yile <admin@8mi.tech>
2023-07-17 19:49:19 +0800

    函数写错

--- 6.ci.sh
+++ 6.ci.sh
@@ -40,8 +40,8 @@ cd aur-all/$GITHUB_REF_NAME
 onekeyGetOnePkgCfg() {
     cd ~/10018-opendde-aur-database/
     file_content="$2"
-    var_list=$(cat $file_content | sed -n '/^[a-zA-Z_][a-zA-Z0-9_]*=/p')
-    save_link="./package/"$(echo $1 | cut -c1)"/"$(echo $1 | awk -F- '{print $1}')"/"$1"/var.sh"
+    var_list=$(echo $file_content | sed -n '/^[a-zA-Z_][a-zA-Z0-9_]*=/p')
+    save_link="./package/"$(echo $1 | cut -c1)"/"$(echo $1 | awk -F- '{print $1}')"/"$1"/var"
     echo $var_list > $save_link
     function_list="$(echo "$file_content" | grep "()" | sed 's#() {##g' | grep -v "^#")"
     for ((i_func = 1; i_func <= $(echo "$function_list" | wc -l); i_func++)); do
@@ -62,7 +62,20 @@ parse_pkgbuild()
     # Todo 解析文件 生成文件到package文件夹
 }
 
-
+onekeyMkdir() {
+    if [ ! -f pkglist.txt ]; then
+        onekeyUpdateList
+    fi
+    for ((i_mkdir = 1; i_mkdir <= $(cat pkglist.txt | wc -l); i_mkdir++)); do
+        pkgname=$(cat pkglist.txt | awk 'NR=='"$i_mkdir"'{print}')
+        save_link="./package/"$(echo $pkgname | cut -c1)"/"$(echo $pkgname | awk -F- '{print $1}')"/"$pkgname
+        mkdir -p $save_link
+        echo "Y:" $save_link
+    done
+}
+onekeyUpdateList() {
+    ./ctl-git-aur --getbranch >pkglist.txt
+}
 
 for pkg in `ls`
 do
