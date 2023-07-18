0f8c7c4abc9ac010e95ca6bb80b6109e436bcd9a
8Mi_Yile <admin@8mi.tech>
2023-07-18 12:10:10 +0800

    fix again

--- build-for-github.sh
+++ build-for-github.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 source ./lib-build.dll.sh
+./lib-change-branch.dll.sh --getbranch >pkglist.txt
 aur_Mkdir-All-Pkg --start "0"
+set -x
 aut_get-All-Pkg-Cfg-Write-To-File --type git --start "0"
