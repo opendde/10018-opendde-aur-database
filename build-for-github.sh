22b31ea7e96eab2691035ef28a1706c1554c692b
8Mi_Yile <admin@8mi.tech>
2023-07-18 12:16:16 +0800

    az

--- build-for-github.sh
+++ build-for-github.sh
@@ -3,5 +3,5 @@
 source ./lib-build.dll.sh
 ./lib-change-branch.dll.sh --getbranch >pkglist.txt
 aur_Mkdir-All-Pkg --start "0"
-set -x
+#set -x
 aut_get-All-Pkg-Cfg-Write-To-File --type git --start "0"
